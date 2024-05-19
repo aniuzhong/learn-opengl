42f7ba54566b6345a5420573c1bb9edda2d776a5
aniuzhong <zxy_9125@163.com>
2024-05-19 15:52:32 +0800

    Create 1.getting_started/1.2.hello_window_clear project

---
+++ src/1.getting_started/1.2.hello_window_clear/glwidget.cpp
@@ -0,0 +1,26 @@
+#include "glwidget.h"
+
+GLWidget::GLWidget(QWidget *parent)
+    : QOpenGLWidget(parent)
+{
+}
+
+GLWidget::~GLWidget()
+{
+}
+
+void GLWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+}
+
+void GLWidget::paintGL()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void GLWidget::resizeGL(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
