bd293cda91a85ab3ca947bf9082e1ed1f22cb675
aniuzhong <zxy_9125@163.com>
2024-05-20 21:30:49 +0800

    Update glwidget.cpp (update GLWidget in real time)

--- src/1.getting_started/1.2.hello_window_clear/glwidget.cpp
+++ src/1.getting_started/1.2.hello_window_clear/glwidget.cpp
@@ -18,6 +18,8 @@ void GLWidget::paintGL()
 {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // QMetaObject::invokeMethod(this, "update", Qt::QueuedConnection);/
 }
 
 void GLWidget::resizeGL(int w, int h)
